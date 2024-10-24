374f39118e510bcad27e60ad8fac69f28a34374e
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-10-24 17:47:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/TQk.mp4"
+    "START_IMG_URL", "https://graph.org/file/2cd8a5138f2f27ea1a140-9789660bca9cbddff6.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://envs.sh/TQk.mp4"
+    "PING_IMG_URL", "https://graph.org/file/0aaa17cab40970904662d-2e66fa0fc2b1f0e2f6.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://envs.sh/q6W.jpg"
+STATS_IMG_URL = "https://graph.org/file/0aaa17cab40970904662d-2e66fa0fc2b1f0e2f6.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
